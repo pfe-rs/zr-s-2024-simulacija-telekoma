4fe59511cc393f2563d3ff92131ce693208edeaf
Teodora Milenkovic <teodora.milenkovic160805@gmail.com>
2024-03-30 15:25:55 +0100

    konacna verzija modulacije

--- modulation.py
+++ modulation.py
@@ -1,33 +1,36 @@
 import numpy as np 
 import math
-import simboli
+from simboli import Symbol
 
 class modulation:
     def __init__(self, qm=None, bit_number=None):
-        if qm.isNone():
+        if qm is None:
             self.bit_number = bit_number
-            self.qm = math.log2(bit_number)
-        if bit_number.isNone():
+            qm = math.log2(bit_number) 
+        if bit_number is None:
             self.qm = qm 
-            self.bit_number = 2**qm
+            self.bit_number = 2**qm 
 
-    3
     def map (self):
         return 
     
 class PSK(modulation):
     def map(self, bits):
+        binary_string = "".join(str(bit) for bit in bits)
+        num = int(binary_string, 2)
         phase_offset = 0 
         symbol_map = np.exp(1j * (2 * np.pi / self.bit_number) * np.arange(self.bit_number) + phase_offset)
-        symbols = symbol_map[bits]
-        return symbol(symbols.real, symbols.imaginary)
+        symbols = symbol_map[num]
+        return Symbol(I=symbols.real, Q=symbols.imag)
+       
 
 class QAM(modulation):
     def map(self, bits):
-        side_length = int(np.sqrt(self.bit_number))
-        symbol_map = np.linspace(-1 + 1j, 1 - 1j, side_length)
-        symbols = symbol_map[bits]
-        return symbol(symbols.real, symbols.imaginary)
+        binary_string = "".join(str(bit) for bit in bits)
+        num = int(binary_string, 2)
+        side_lenght = self.qm 
+        edge = side_lenght-1
+        return Symbol(I=(num//side_lenght)*2-edge, Q=(num%side_lenght)*2-edge)
 
 
         
