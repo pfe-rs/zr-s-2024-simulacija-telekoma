d2fbff50a500303d49a495ee1f05c8c912a753ac
Teodora Milenkovic <teodora.milenkovic160805@gmail.com>
2024-03-30 15:14:10 +0100

    konacna verzija mapera

--- mapper1.py
+++ mapper1.py
@@ -6,16 +6,21 @@ import simboli
 
 class Mapper:
     def __init__(self, modulation):
-        self.modulaation = modulation 
-        
+        self.modulation = modulation  
+   
     def split_input_bits(self, input_bits, step):
      return [input_bits[i:i+step] for i in range(0, len(input_bits), step)]
 
     def step(self, input_bits, step):
-        return modulation.map(self.split_input_bits(input_bits, step))
+        words_array = self.split_input_bits(input_bits, step)
+        if self.modulation == 'PSK':    
+            return [modulation.PSK(qm = step).map(word) for word in words_array]
+        else:
+            return [modulation.QAM(qm = step).map(word) for word in words_array]
+        
 
-    def plot_constellation(self, input_bits):
-        symbols = self.step(input_bits, math.log2(len(input_bits)))
+    def plot_constellation(self, input_bits, Qm):
+        symbols = self.step(input_bits, Qm)
         I_values = [symbol.I for symbol in symbols]
         Q_values = [symbol.Q for symbol in symbols]
         plt.scatter(I_values, Q_values, marker="o", color="b")
